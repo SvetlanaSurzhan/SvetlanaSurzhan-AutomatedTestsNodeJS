9fa953c2857dff0bb40ac2a5ee2e07c79c413114
Svetlana Surzhan <svetlana.d.surzhan>
2022-09-21 20:33:29 -0400

    Add existing project files to Git

---
+++ src/app.js
@@ -0,0 +1,15 @@
+function Greetings(name) {
+    console.log('Hi ' + name);
+}
+Greetings('Sveta');
+
+function Reminder(question) {
+    console.log(question);
+}
+Reminder('Do you remamber what is Node.js?????');
+
+function Answer(paragraph) {
+   console.log(paragraph); 
+}
+Answer('Node is a runtime environment that allows developers to execute JavaScript code outside the browser, on the server-side.');
+
