17cc959ac224622ada32d01d66303019033b8aa4
Svetlana Surzhan <svetlana.d.surzhan>
2022-09-27 20:31:36 -0400

    deleted all falders for jasmin project and fixed fizzBuzzSpec

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function fizzBuzz(number) {
+    if (((number % 3) === 0 && (number % 5)) === 0){
+        return('fizzBuzz');
+    }
+    else if ((number % 5) === 0){
+        return('buzz');
+    }
+    else if((number % 3) === 0){
+        return('fizz');
+    }
+    else{
+        return number;
+    } 
+}
+
+module.exports = fizzBuzz;
+
+
---
+++ src/app.test.js
@@ -0,0 +1,29 @@
+const fizzBuzz = require('./app')
+
+function testFizzBuzz() {
+    if (fizzBuzz(3) === 'fizz') {
+        console.log('3 - Pass');
+    } else {
+        console.log('3 - Fail');
+    }
+
+    if (fizzBuzz(5) === 'buzz') {
+        console.log('5 - Pass');
+    } else {
+        console.log('5 - Fail');
+    }
+
+    if (fizzBuzz(15) === 'fizzBuzz') {
+        console.log('15 - Pass');
+    } else {
+        console.log('15 - Fail');
+    }
+    
+    if (fizzBuzz(7) === 7) {
+        console.log('7 - Pass');
+    } else {
+        console.log('7 - Fail');
+    }
+}
+
+testFizzBuzz()
