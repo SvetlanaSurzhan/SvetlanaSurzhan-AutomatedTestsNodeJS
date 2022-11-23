dd059540e2b38ab9f6ea4d150d602baa34a429f6
Svetlana Surzhan <svetlana.d.surzhan>
2022-11-22 21:24:15 -0500

    created post request for new checklist

--- spec/getReqPublicApi.js
+++ spec/getReqPublicApi.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 
 // GET test:
 describe('GET request', () => {
+
+
     it('Check body', (done) => {
         axios.get('https://www.anapioficeandfire.com/api/characters')
             .then(function (response) {
